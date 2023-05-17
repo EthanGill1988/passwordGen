4d594d276b2eb89f3695fb35aedcabbe343f4c35
Ethan Gill <1ethangill1@gmail.com>
2023-05-16 19:22:59 -0700

    Tutor help iron out ish

--- scrypt.js
+++ scrypt.js
@@ -6,19 +6,50 @@ function generatePassword() {
   
 let text;
 let input0 = prompt("Select between 8 and 128", "Enter Here");
-let input1 = prompt("Do you wish to include lowercase letters?");
-let input2 = prompt
+if (input0 < 8) {
+    alert("Please Select a Number Higher than Eight");
+    return "Please Try Again";
+}
+if (input0 > 128) {
+    alert("Please Select a Number Lower than 128");
+    return "Please Try Again";
+}
+if (isNaN(input0)) {
+    alert("Must be a Number!");
+    return "Please Try Again";
+}
+let charset = ""
+let input1 = confirm("Do you wish to include lowercase letters?");
+if (input1 === true) {
+    charset += "abcdefghijklmnopqrstuvwxyz";
+}
 
-var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+let input2 = confirm("Do you wish to include uppercase letters?");
+if (input2 === true) {
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+let input3 = confirm("Do you wish to include special figures?");
+if (input3 === true) {
+    charset += "!@#$%^&*()_-=?/";
+}
+ let input4 = confirm("Do you wish to include numbers?");
+ if (input4 === true) {
+    charset += "0123456789";
+ }
+
+console.log(charset);
+
+// var length = 8,
+//         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+//         retVal = "";
+//     for (var i = 0, n = charset.length; i < length; ++i) {
+//         retVal += charset.charAt(Math.floor(Math.random() * n));
+//     }
+//     return retVal;
   
   return "Generated Password"
 }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -28,6 +59,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
